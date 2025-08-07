f337d60855842ca5963c9731e96bb0234d421daa
Julian <juliandavidmogollon0803@gmail.com>
2025-08-07 11:42:09 -0500

    Se creo el moodulo de detalle_instructor

--- sena_app/instructores/views.py
+++ sena_app/instructores/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Instructor
@@ -9,5 +9,17 @@ def instructores(request):
     context = {
         'lista_instructores': lista_instructores,
         'total_instructores': lista_instructores.count(),
+    }
+    return HttpResponse(template.render(context, request))
+
+def detalle_instructor(request, instructor_id):
+    instructor = get_object_or_404(Instructor, id=instructor_id)
+    cursos_coordinados = instructor.cursos_coordinados.all()
+    cursos_impartidos = instructor.cursos_impartidos.all()
+    template = loader.get_template('detalle_instructor.html')
+    context = {
+        'instructor': instructor,
+        'cursos_coordinados': cursos_coordinados,
+        'cursos_impartidos': cursos_impartidos,
     }
     return HttpResponse(template.render(context, request))
